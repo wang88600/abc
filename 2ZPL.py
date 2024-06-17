5549b5c55a419d637bfeae6fd5623a41d97268f5
wang88600 <zyx88600@gmail.com>
2024-06-17 23:49:04 +0800

    Initial commit

---
+++ 2ZPL.py
@@ -0,0 +1,31 @@
+import serial
+
+# 打開串口
+ser = serial.Serial('COM1', 9600, timeout=1) # 是印表機可以用串口、網卡等連接開發。我選擇用串口了
+
+# 設定ZPL指令
+zpl_command = '''
+^XA
+
+^FO50,50^GB500,350,5^FS
+
+^CF0,40
+^FO100,95^FDABCDE-12345^FS
+^FO100,145^FDabcde-12345^FS
+^FO100,195^FDABCDE-12345-abcde^FS
+^FO50,250^GB500,3,5^FS
+
+^BY3,3,50
+^FO110,280^BC^FD12345678^FS
+
+^XZ
+'''
+
+# 將ZPL指令轉換為位元組數組
+zpl_bytes = zpl_command.encode('utf-8')
+
+# 發送ZPL指令到印表機
+ser.write(zpl_bytes)
+
+# 關閉串口
+ser.close()
